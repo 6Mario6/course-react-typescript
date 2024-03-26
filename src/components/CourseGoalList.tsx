f868158bffb1d77e084ae3f4558bda2ae25ead37
6Mario6 <mario66fernandez@gmail.com>
2024-03-25 23:11:15 -0500

    Add new component CourseGoalList

---
+++ src/components/CourseGoalList.tsx
@@ -0,0 +1,18 @@
+import CourseGoal from "./CourseGoal";
+import { type CourseGoal as CGoal } from "../App";
+type CourseGoalListProps = {
+  goals: CGoal[];
+};
+export default function w({ goals }: CourseGoalListProps) {
+  return (
+    <ul>
+      {goals.map((goal) => (
+        <li key={goal.id}>
+          <CourseGoal title={goal.title}>
+            <p>{goal.description}</p>
+          </CourseGoal>
+        </li>
+      ))}
+    </ul>
+  );
+}
